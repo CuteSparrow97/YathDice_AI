e6e28037b35d3e2120d2d374379cc7ad0e5aaa2b
Qute_Sparrow <dlckdgml345@naver.com>
2021-05-09 22:37:28 +0900

    TKINTER를 이용한 족보 출력

--- YACHT_DICE.py
+++ YACHT_DICE.py
@@ -1,4 +1,6 @@
 import random
+import tkinter
+import tkinter.ttk
 
 # 야추 다이스에는 총 2명의 Player가 존재한다.
 # Player가 가지고 있는 정보 : 점수, 주사위 던진 횟수, 먹은 족보 , 턴수
@@ -28,25 +30,31 @@ class Player:
     def tumble_Dice(self):
         print("주사위를 돌리셨습니다.")
         for Dice in self.Dices:
-            if Dice.bHolding:
+            if not Dice.bHolding:
                 Dice.tumble()
         self.nCounts = self.nCounts + 1
 
     # 주사위 값 홀딩 시키기
-    def Holding_Dice(self, nDice):
-        self.Dices[nDice].Holding(True)
-    def Holding_Dice(self, nDice,nDice2):
-        self.Dices[nDice].Holding(True)
-        self.Dices[nDice2].Holding(True)
-    def Holding_Dice(self, nDice, nDice2,nDice3):
-        self.Dices[nDice].Holding(True)
-        self.Dices[nDice2].Holding(True)
-        self.Dices[nDice3].Holding(True)
-    def Holding_Dice(self, nDice, nDice2, nDice3,nDice4):
-        self.Dices[nDice].Holding(True)
-        self.Dices[nDice2].Holding(True)
-        self.Dices[nDice3].Holding(True)
-        self.Dices[nDice4].Holding(True)
+    # 오버로딩 안되서 입력값 유무를 판단하여 Holding
+    def Holding_Dice(self, nDice , nDice2 = 0, nDice3 = 0, nDice4= 0):
+        if nDice2 == 0 and nDice3 == 0 and nDice4 == 0:
+            self.Dices[nDice].Holding(True)
+
+        elif nDice3 == 0 and nDice4 ==0:
+            self.Dices[nDice].Holding(True)
+            self.Dices[nDice2].Holding(True)
+
+        elif nDice4 == 0:
+            self.Dices[nDice].Holding(True)
+            self.Dices[nDice2].Holding(True)
+            self.Dices[nDice3].Holding(True)
+
+        else:
+            self.Dices[nDice].Holding(True)
+            self.Dices[nDice2].Holding(True)
+            self.Dices[nDice3].Holding(True)
+            self.Dices[nDice4].Holding(True)
+
 
 # 주사위 만들기
 class Dice:
@@ -66,7 +74,7 @@ class Dice:
 # 초기값  설정
 nScore = 0
 nChance = 3
-dHandRankings = {'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'all':0}
+dHandRankings = {'1':2,'2':0,'3':0,'4':0,'5':0,'6':0,'all':0}
 nTurn = 0
 
 # Player1 생성
@@ -77,13 +85,59 @@ Player1 = Player(nScore, nChance, dHandRankings, nTurn)
 Player1.PrintDiceValue()
 
 # 주사위를 굴린다.
-Player1.tumble_Dice()
-Player1.PrintDiceValue()
+# Player1.tumble_Dice()
+# Player1.PrintDiceValue()
+# Player1.Holding_Dice(3,4)
+# Player1.tumble_Dice()
+# Player1.PrintDiceValue()
+# print(dHandRankings)
+
+# GUI창을 생성하고 라벨을 설정한다.
+root = tkinter.Tk()
+root.title("Yatch Dice")
+root.geometry("540x300+100+100")
+root.resizable(False,False)
+
+lbl = tkinter.Label(root, text="Player1")
+lbl.pack()
+
+# 표 생성하기. coulms는 컬럼 이름, displaycolums는 실행될 때 보여지는 순서이다.
+treeview = tkinter.ttk.Treeview(root, columns=["one"], displaycolumns = ["one"])
+treeview.pack()
+
+# 각 컬럼 설정. 컬럼 이름, 컬럼 넓이, 정렬 등
+treeview.column("#0", width = 100, anchor = "center")
+treeview.heading("#0", text="족보")
+
+treeview.column("#1", width = 100, anchor = "center")
+treeview.heading("one", text="값", anchor = "center")
+
+# 표에 데이터 삽입
+for key, value in dHandRankings.items():
+    treeview.insert('','end',text = key, values = value,iid=key)
+
+# GUI 실행
+root.mainloop()
+
+
+# tkinter를 이용한 gui 출력
+
 
 ### 진행중 ###
 # YACHT_DICE 게임 만들기
+# 족보 패 표로 출력하기
 
 ### 예정 ###
 # Player1 AI 만들기
 # FullHouse / small Straight / Big Straight / YACHT 패 추가
 # Player2 추가 예정
+
+### 완료 ###
+'''
+Player 만들기
+주사위 만들기
+주사위 던지기 기능
+주사위 홀딩 기능
+
+
+'''
